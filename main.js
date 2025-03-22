3cb069ad584e4e696837ac306f9e42c43abe72d9
Courtney Portnoy <Courtney.Portnoy@Courtney.Portnoy-CDW7LXDL4V>
2025-03-21 17:30:40 -0700

    updates

--- main.js
+++ main.js
@@ -4,47 +4,42 @@ document.addEventListener('DOMContentLoaded', function() {
     let links = aside.getElementsByClassName('li-link');
     let subMenus = aside.getElementsByClassName('sub-menu');
 
-    // Function to close all sub menus
     function closeAllSubMenus() {
-        Array.from(subMenus).forEach(menu => menu.style.maxHeight = '0px');
+        Array.from(subMenus).forEach(menu => {
+            menu.style.maxHeight = '0px';
+            let parentLink = menu.previousElementSibling;
+            if (parentLink) {
+                parentLink.classList.remove('expanded');
+            }
+        });
     }
 
-    // Function to update the h1 text with the text from the span
     function updateTitle(newTitle) {
         const mainTitle = document.getElementById('main-title');
-        console.log(mainTitle);
-        console.log(newTitle);
         mainTitle.textContent = newTitle;
     }
 
-
-    // Add click event listeners to all nav items
     Array.from(navItems).forEach(navItem => {
         let link = navItem.querySelector('.li-link');
         let newTitle = navItem.querySelector('.item-label');
-        link.addEventListener('click', function(event) {
-            let subMenu = navItem.querySelector('.sub-menu');
+        let subMenu = navItem.querySelector('.sub-menu');
 
-            // Check if the current nav item has a sub menu
+        link.addEventListener('click', function(event) {
             if (subMenu) {
                 let isExpanded = subMenu.style.maxHeight && subMenu.style.maxHeight !== '0px';
                 closeAllSubMenus();
-                subMenu.style.maxHeight = isExpanded ? '0px' : subMenu.scrollHeight + 'px';
+                subMenu.style.maxHeight = subMenu.scrollHeight + 'px';
+                link.classList.add('expanded');
                 event.preventDefault();
             } else {
-                closeAllSubMenus(); // Close all sub menus if a non-accordion item is clicked
+                closeAllSubMenus();
             }
-
-            // Remove active class from all links and add to the clicked link
-            Array.from(links).forEach(link => link.classList.remove('active', 'child-active'));
+            Array.from(links).forEach(l => l.classList.remove('active', 'child-active'));
             link.classList.add('active');
-
-            // Update the main title
             updateTitle(newTitle.textContent);
         });
-    }); 
+    });
 
-    // Add click event listeners to all sub menu links
     Array.from(subMenus).forEach(subMenu => {
         let subLinks = subMenu.getElementsByClassName('li-link');
         Array.from(subLinks).forEach(subLink => {
@@ -52,7 +47,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 let newTitle = subLink.querySelector('.item-label');
                 Array.from(links).forEach(link => link.classList.remove('active', 'child-active'));
                 subLink.classList.add('active', 'child-active');
-                // Update the main title
                 updateTitle(newTitle.textContent);
                 event.stopPropagation();
             });
